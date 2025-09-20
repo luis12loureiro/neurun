1526376d4c32c37699ba71c71ab95961ba1182a9
luis12loureiro <luis12loureiro.dev@gmail.com>
2025-09-20 22:28:36 +0100

    add tasks to memo data

--- internal/workflow/repository/storage/data.go
+++ internal/workflow/repository/storage/data.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"time"
+
 	"github.com/luis12loureiro/neurun/internal/workflow/domain"
 )
 
@@ -10,7 +12,32 @@ var DefaultWorkflows = map[string]domain.Workflow{
 		Name:        "Default Workflow 1",
 		Description: "This is the first default workflow",
 		Status:      domain.WorkflowStatusIDLE,
-		Tasks:       []*domain.Task{},
+		Tasks: []*domain.Task{
+			{
+				ID:         "task1",
+				Name:       "Task 1",
+				Type:       domain.TaskTypeLog,
+				Status:     domain.TaskStatusPending,
+				Retries:    1,
+				RetryDelay: 2 * time.Second,
+				Payload: &domain.LogPayload{
+					Message: "Hello, World!",
+				},
+				Next: []*domain.Task{
+					{
+						ID:         "task2",
+						Name:       "Task 2",
+						Type:       domain.TaskTypeLog,
+						Status:     domain.TaskStatusPending,
+						Retries:    2,
+						RetryDelay: 3 * time.Second,
+						Payload: &domain.LogPayload{
+							Message: "My name is Luís!",
+						},
+					},
+				},
+			},
+		},
 	},
 	"wf2": {
 		ID:          "wf2",
