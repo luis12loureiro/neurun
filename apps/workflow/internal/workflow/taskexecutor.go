28bd13bda9b53223be603ffebae4b026494d2338
luis12loureiro <luis12loureiro.dev@gmail.com>
2025-12-05 23:44:49 +0000

    restructure the project to add the frontend

---
+++ apps/workflow/internal/workflow/taskexecutor.go
@@ -0,0 +1,56 @@
+package workflow
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/luis12loureiro/neurun/apps/workflow/internal/workflow/domain"
+)
+
+type TaskExecutor interface {
+	Execute(ctx context.Context, t *domain.Task) (interface{}, error)
+}
+
+type taskExecutor struct {
+	// httpExecutor HTTPTaskExecutor TODO: Add HTTPTaskExecutor
+	// logExecutor  LogTaskExecutor TODO: Add LogTaskExecutor
+}
+
+func NewTaskExecutor() TaskExecutor {
+	return &taskExecutor{
+		// httpExecutor: NewHTTPTaskExecutor(), TODO: Add HTTPTaskExecutor
+		// logExecutor:  NewLogTaskExecutor(),  TODO: Add LogTaskExecutor
+	}
+}
+
+func (te *taskExecutor) Execute(ctx context.Context, t *domain.Task) (interface{}, error) {
+	t.Status = domain.TaskStatusRunning
+
+	var err error
+	var output interface{}
+	switch t.Type {
+	case domain.TaskTypeHTTP:
+		return nil, fmt.Errorf("HTTP task executor not implemented yet")
+	case domain.TaskTypeLog:
+		logPayload, ok := t.Payload.(*domain.LogPayload)
+		if !ok {
+			err = fmt.Errorf("invalid payload type for LOG task")
+		} else {
+			randomSeconds := rand.Intn(3)
+			time.Sleep(time.Duration(randomSeconds) * time.Second)
+			output = logPayload.Message
+		}
+	default:
+		err = fmt.Errorf("unknown task type: %v", t.Type)
+	}
+
+	if err != nil {
+		t.Status = domain.TaskStatusFailed
+		return nil, err
+	} else {
+		t.Status = domain.TaskStatusCompleted
+	}
+	return output, nil
+}
