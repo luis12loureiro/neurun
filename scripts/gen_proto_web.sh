d6ce32e7180f342cc52a7311463b1a6dc410d087
luis12loureiro <luis12loureiro.dev@gmail.com>
2025-12-08 15:57:33 +0000

    fix proto

--- scripts/gen_proto_web.sh
+++ scripts/gen_proto_web.sh
@@ -6,7 +6,11 @@ OUT_DIR="../apps/frontend/src/app/proto-gen"
 mkdir -p $OUT_DIR
 rm -rf $OUT_DIR/*
 
-protoc -I=$PROTO_DIR \
-  --js_out=import_style=commonjs:$OUT_DIR \
-  --grpc-web_out=import_style=typescript,mode=grpcwebtext:$OUT_DIR \
+# Use protoc bundled with grpc-web (not system protoc)
+PROTOC=../apps/frontend/node_modules/.bin/protoc
+GRPC_WEB_PLUGIN=../apps/frontend/node_modules/.bin/protoc-gen-grpc-web
+
+$PROTOC -I=$PROTO_DIR \
+  --js_out=import_style=commonjs,binary:$OUT_DIR \
+  --grpc-web_out=import_style=typescript,mode=grpcweb:$OUT_DIR \
   $PROTO_DIR/*.proto
